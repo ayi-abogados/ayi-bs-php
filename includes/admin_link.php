804cbcbb36c869903f5d8e282b8a4c825222e879
geidyestrada-abogados <torra@automaticaintegral.com>
2025-01-20 19:22:29 -0500

    Add files via upload

--- includes/admin_link.php
+++ includes/admin_link.php
@@ -79,7 +79,7 @@ $modalHtml = <<<HTML
     <div class="modal fade" id="loginModal" tabindex="-1" aria-labelledby="loginModalLabel" aria-hidden="true">
         <div class="modal-dialog modal-dialog-centered">
             <div class="modal-content">
-                <div class="modal-header">
+                    <div class="modal-header">
                     <h5 class="modal-title" id="loginModalLabel">Inicio de Sesión</h5>
                     <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                 </div>
